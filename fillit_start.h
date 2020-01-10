72d0485420f40b19f8387b72bf7a8a17e8e276b3
fwiley <silin.nv@yandex.ru>
2020-01-10 19:30:27 +0300

    validation

--- fillit_start.h
+++ fillit_start.h
@@ -5,36 +5,26 @@
 #ifndef FILLIT_START_H
 # define FILLIT_START_H
 # include "libft/libft.h"
-# include "get_next_line.h"
 # include <stdlib.h>
 # include <fcntl.h>
 
-typedef struct  tetramino_s
+typedef struct  		tetramino_s
 {
-    __uint128_t         map;
-    struct tetramino_s  *next;
-    int                 width;
-    char                letter;
-}               tetramino;
+	__uint128_t			map;
+	struct tetramino_s	*next;
+	int					width;
+	char				letter;
+}						tetramino;
 
-typedef struct  matrix_neg_s
+typedef struct			matrix_neg_s
 {
-    __uint128_t         right_border;
-    __uint128_t         bottom_border;
-    __uint128_t         negative;
-}               matrix_neg;
+	__uint128_t			right_border;
+	__uint128_t			bottom_border;
+	__uint128_t			negative;
+}						matrix_neg;
 
-tetramino       *test_map(int map_size, int type_fig);
-void            print_map(__uint128_t map, int map_size);
-int             shift(tetramino *fig, int map_size, __uint128_t right_border, __uint128_t bottom_border);
-int             test(tetramino *fig, int map_size, __uint128_t negative);
-void            on_map(tetramino *fig, int map_size);
-matrix_neg      save_borders(int map_size);
-void            print_uint(__uint128_t line, int size, char l);
-int             val_chk(char *pos_f);
-int             sym_chk(char *line);
-tetramino       *fig_to_tet(char *pos_fig, int fig_counter);
-int             print_error(int p);
-tetramino		*get_tetramino(int fd, char letter);
 
-#endif //UNTITLED_FILLIT_START_H
+int						print_error(int p);
+tetramino				*get_tetramino(int fd, char letter);
+
+#endif
--- read_check.c
+++ read_check.c
@@ -2,42 +2,44 @@
 
 int     print_error(int p)
 {
-
-    if (p == 2)
-        ft_putstr("error\n");
-    if (p == 1)
-        ft_putstr("usage: ./fillit source_file\n");
-    return (0) ;
+	if (p == 2)
+		ft_putstr("error\n");
+	else
+		ft_putstr("usage: ./fillit source_file\n");
+	return (0);
 }
 
-int     solve(int fd, char *figure)
+int		solve(int fd)
 {
-    tetramino *fig;
+	tetramino *fig;
 
-    if ((fig = get_tetramino(fd, 'A')))
-        return (1);
-    return (0);
+	if ((fig = get_tetramino(fd, 'A')))
+		return (1);
+	return (0);
 }
 
-int     main(int ac, char **av)
+int		main(int ac, char **av)
 {
-    int fd;
+	int		fd;
+	int		read_var;
+	char	*str;
 
-    if (ac == 2)
-    {
-        fd = open(av[1], O_RDONLY);
-        if (read(fd, av[1], 0) == 0)
-        {
-            if (solve(fd, av[1]))
-            {
-                close(fd);
-                return (0);
-            }
-        }
-        close(fd);
-        print_error(2);
-        return (-1);
-    }
-    print_error(1);
-    return (-1);
+	str = av[1];
+//	ft_putstr(str);
+	if (ac == 2)
+	{
+		fd = open(str, O_RDONLY);
+		read_var = read(fd, str, 0);
+		if (read_var == 0)
+		{
+			if (solve(fd))
+			{
+				close(fd);
+				return (0);
+			}
+		}
+		close(fd);
+	}
+	print_error(ac);
+	return (-1);
 }
